8a1e6ceb5c3f4aca332a2b29575955cc54f0b4ef
Senada Kadric <skadric@fordham.edu>
2020-02-25 22:50:13 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -23,10 +23,6 @@ end
 
 
 def now_serving(array)
-  if array.length == 0
-  puts "There is nobody waiting to be served!"
-  else
-  puts "Currently serving #{array[0]}."
-  array.shift
+  array.length == 0 ? (puts "There is nobody waiting to be served!") : (puts "Currently serving #{array[0]}." array.shift)
   end
 end
